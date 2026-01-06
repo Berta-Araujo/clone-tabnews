4d3bda1701721f6ac06cd1a2f9b6f35818fa3219
Berta Araujo <berta_araujo@live.com>
2026-01-06 22:28:32 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
     function Home () {
-        return <h1> Amo Você &hearts; </h1>
+        return <h1> Amo Você, se você me ama, dá uma risadinha. &hearts; </h1>
     }
 
     export default Home; 
